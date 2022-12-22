525e54ca133cda77962dd4b75460572d97465ef0
Amanda Forrester <amanda.forrester1@gmail.com>
2022-12-22 10:14:44 -0600

    Got rid of some unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          This is the app!
+        </p>
+        <label for="gratitude">What are you grateful for?</label>
+        <textarea id="gratitude" name="gratitude" rows="4" cols="50"></textarea>
         <a
           className="App-link"
           href="https://reactjs.org"
