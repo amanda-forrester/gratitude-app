1f9843d0ae90fb03c34bb33893b94f524ee764af
Amanda Forrester <amanda.forrester1@gmail.com>
2023-06-13 14:58:04 -0500

    reversed changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import LoginPage from './Routes/LoginPage';
 import SuccessPage from './Routes/SuccessPage';
 import { BrowserRouter, Routes , Route } from 'react-router-dom';
-const frontendUrl = "https://gratitude-app.onrender.com";
 
 function App() {
   return (
@@ -10,8 +9,8 @@ function App() {
         <header className="App-header">
           <BrowserRouter>
             <Routes>
-              <Route path='/' element={ <LoginPage /> } />
-              <Route path='https://gratitude-app.onrender.com/success' element={ <SuccessPage />} />
+              <Route path="/" element={ <LoginPage /> } />
+              <Route path="/success" element={ <SuccessPage />} />
             </Routes>
           </BrowserRouter>
         </header>
