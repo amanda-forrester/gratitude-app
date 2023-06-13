1a35778c0ab402854dc4b91457555ea0ee2df9c5
Amanda Forrester <amanda.forrester1@gmail.com>
2023-06-13 14:31:14 -0500

    added ssl config

--- backend/queries.js
+++ backend/queries.js
@@ -6,6 +6,9 @@ dotenv.config();
 const Pool = require('pg').Pool;
 const pool = new Pool( {
     connectionString: process.env.CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 
 
