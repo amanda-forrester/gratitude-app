cb51dd4de5ae15fef672da606e53137eed246501
Amanda Forrester <amanda.forrester1@gmail.com>
2023-06-23 12:40:50 -0500

    reworked style structure

--- frontend/src/Routes/LoginPage.js
+++ frontend/src/Routes/LoginPage.js
@@ -4,7 +4,7 @@ import LoginButton from '../Auth.js';
 function LoginPage() {
     return (
         <div>
-            <h1>Welcome to the Gratitude App! We are grateful you are here.</h1>
+            <h1>Welcome to the Gratitude App! <br></br>We are grateful you are here.</h1>
             {LoginButton()}
         </div>
     )
